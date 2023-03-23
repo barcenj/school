781f3dc6796f1b5ed7ec18e69fbbf6d48cda4817
Joshua Barcenas <barcenasj@ufl.edu>
2023-03-22 22:25:48 -0400

    Added notes

--- Software Engineering.py
+++ Software Engineering.py
@@ -8,18 +8,25 @@ def menu():
 
 # encrypts password
 def encode(password):
+    # convert int to string (assuming I made the input an int LOL I changed that)
     password = f'{password}'
     encode_pass = []
     for i in range(len(password)):
+        # encryption operation is + 3 to each dig
         string = int(password[i]) + 3
+
+        # Fix instances out of range (0,9)
         if string == 10:
             string = 0
         elif string == 11:
             string = 1
         elif string == 12:
             string = 2
+
+        # convert back to string
         string = f'{string}'
         encode_pass.append(string)
+
     encode_pass = ''.join(encode_pass)
     return encode_pass
 
@@ -41,12 +48,14 @@ if __name__ == '__main__':
         menu()
         op = input('Please enter an option: ')
 
+        # encoding tool
         if op == '1':
             password = input('Please enter your password to encode: ')
             encode(password)
             print('Your password has been encoded and stored!')
             print()
 
+        # decoding tool
         elif op == '2':
             orig_pass = decode(encode(password))        # implementing decode function into main
             print(f'The encoded password is {encode(password)}, and the original password is {orig_pass}')
@@ -54,5 +63,6 @@ if __name__ == '__main__':
         elif op == '3':
             break
 
+        # screw error messages ig idk
         else:
             pass
